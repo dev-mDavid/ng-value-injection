650fcf68e139325ee9f74a9dd9eefa00c00fb56f
mDavid <dev.mdavid@gmail.com>
2019-12-25 20:05:34 -0800

    provide Analytics Service in Analytics Demo Module

--- src/app/analytics-demo/analytics-demo.module.ts
+++ src/app/analytics-demo/analytics-demo.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { AnalyticsService } from "../services/analytics-service.service";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    AnalyticsService
   ]
 })
 export class AnalyticsDemoModule { }
