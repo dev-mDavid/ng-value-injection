7644e4dc808183bbbe0d547b4992157f14a03821
mDavid <dev.mdavid@gmail.com>
2019-12-25 20:08:33 -0800

    config Analytics Demo Interface

--- src/app/analytics-demo/analytics-demo-interface.ts
+++ src/app/analytics-demo/analytics-demo-interface.ts
@@ -1,2 +1,9 @@
-export interface AnalyticsDemoInterface {
+export interface Metric {
+  eventName: string;
+  scope: string;
+
+}
+
+export interface AnalyticsImplementation {
+  recordEvent(metric: Metric): void;
 }
